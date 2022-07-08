929e2694ec71bd58b8eb8f4469b4143bb248a1a3
caiocipriano <caio.silvax@hotmail.com>
2022-07-08 17:41:59 -0300

    test: new routes for movies

--- test/movie-tests/movie.spec.ts
+++ test/movie-tests/movie.spec.ts
@@ -4,8 +4,12 @@ import { app } from "../../src/server";
 
 describe("",()=>{
     const prisma = new PrismaClient()
+   test("Should return all movies",async()=>{
+        const response = await request(app).get("/movies")
+        expect(response.status).toBe(200)
+    })
 
-    test("Should create movie",async()=>{
+   test("Should create movie",async()=>{
         const response = await request(app).post("/movies")
         .send({
             title:"test",
@@ -14,5 +18,15 @@ describe("",()=>{
             nameTitle: "Ação"
         })
         expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty('Id')
     })
+
+    test("Should return one movie",async()=>{
+        const response = await request(app).get("/movies/1")
+        expect(response.body).toHaveProperty("Id")
+        expect(response.status).toBe(200)
+    })
+
+
+    
 })
